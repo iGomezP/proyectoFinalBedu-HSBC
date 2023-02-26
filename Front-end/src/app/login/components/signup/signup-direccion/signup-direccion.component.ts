e01fd5d7abd1b725ff04083329b4ea6d5e640ded
iGomezP <dev.i.gomezp@outlook.com>
2023-02-26 08:28:02 -0600

    Fix #52

---
+++ Front-end/src/app/login/components/signup/signup-direccion/signup-direccion.component.ts
@@ -0,0 +1,58 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { AuthService } from 'src/app/services/api/auth.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+
+@Component({
+  selector: 'app-signup-direccion',
+  templateUrl: './signup-direccion.component.html',
+  styleUrls: ['./signup-direccion.component.scss'],
+})
+export class SignupDireccionComponent implements OnInit {
+  @Input() inputData: any;
+  usuario: UsuarioModel = {
+    name: '',
+    email: '',
+    userPassword: '',
+    direccion: {
+      calle: '',
+      numeroInterior: '',
+      numeroExterior: '',
+      ciudad: '',
+      estado: '',
+      codigoPostal: '',
+      colonia: '',
+      referencias: '',
+    },
+  };
+
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private snackbarService: SnackbarService
+  ) {}
+  ngOnInit(): void {
+    this.usuario.name = this.inputData.username;
+    this.usuario.email = this.inputData.email;
+    this.usuario.userPassword = this.inputData.password;
+  }
+
+  // signupBackwards() {
+  //   console.log('enviando datos de regreso');
+  // }
+
+  signup() {
+    console.log('Haciendo post para el registro');
+    this.authService.apiRegister(this.usuario).subscribe({
+      next: (res) => {},
+      error: (error) => console.log(error),
+      complete: () => {
+        this.snackbarService.printBienvenida(
+          'Registro exitoso, por favor inicia sesión'
+        );
+        this.router.navigate(['/login']);
+      },
+    });
+  }
+}
