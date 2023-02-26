f97b99a089155a7e3aa22d3ee3619ce10db818da
iGomezP <dev.i.gomezp@outlook.com>
2023-02-26 08:37:12 -0600

    Fix Netlify

---
+++ Front-end/src/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_SERVER: process.env['API_SERVER'],
+  JWT_USER: process.env['JWT_USER'],
+  JWT_TOKEN: process.env['JWT_TOKEN'],
+  JWT_ROLE: process.env['JWT_ROLE'],
+};
