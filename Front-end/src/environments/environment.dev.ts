8dafe15935b3945ae8f164b78f3df613aa5d3d16
iGomezP <dev.i.gomezp@outlook.com>
2023-02-28 19:11:15 -0600

    Se terminan README y algunas corecciones en nombres

---
+++ Front-end/src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  //API_URL: 'http://192.168.0.186:9117/APIGeros/api/',
+  API_URL: 'http://189.234.176.58/APIGeros/api/',
+  //API_URL: 'http://localhost:8080/api/',
+};
